f9f06a4875f69240eb95643bf74a5d76066df675
nanjingchj <nanjingchj@nanjingchj-TM1604>
2019-10-19 15:42:04 -0400

    init

---
+++ src/mcv4u/problem/set/generator/Problem.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mcv4u.problem.set.generator;
+
+/**
+ *
+ * @author nanjingchj
+ */
+public class Problem {
+    
+}
